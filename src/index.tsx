b0d2591bf6a881f2bff639a0f62ef7a0921f964a
岡本菜那 <nakmtn07@gmail.com>
2024-02-25 22:22:56 +0900

    Fix fonts

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import "./index.css";
 import App from "./App";
 import { DataStoreContextProvider } from "./utils/DataStoreContext";
 import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <DataStoreContextProvider>
         <App />
       </DataStoreContextProvider>
